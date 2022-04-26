b1c2eb8ed01aa3a0e32e2b968fb66ef12ff694a5
Fernando Pereira França França <soufadofernando@gmail.com>
2022-04-25 21:31:47 -0300

    Created a new class

--- AulaTeste2.java
+++ AulaTeste2.java
@@ -1,9 +1,7 @@
-public class AulaTeste {
-	
+public class AulaTeste2{
+
 	public static void main(String[] args){
 		for (int i = 0; i <= 10; i++)
-			System.out.println(i);
+		System.out.println(i);
 }
 }
-
-	
